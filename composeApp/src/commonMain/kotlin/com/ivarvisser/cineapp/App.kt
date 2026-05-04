b5b02d879d1e84744c7865a8efe0d60e2a43c483
ivarv <ivarvisser@hotmail.com>
2026-05-04 16:46:50 +0200

    CAP-10 added themeing colors for cine comp

--- composeApp/src/commonMain/kotlin/com/ivarvisser/cineapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/ivarvisser/cineapp/App.kt
@@ -1,28 +1,13 @@
 package com.ivarvisser.cineapp
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import cineapp.composeapp.generated.resources.Res
-import cineapp.composeapp.generated.resources.compose_multiplatform
+import com.ivarvisser.cineapp.theming.CineAppTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    CineAppTheme(darkTheme = true) {
         CineApp()
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/ivarvisser/cineapp/theming/BrandColors.kt
@@ -0,0 +1,18 @@
+package com.ivarvisser.cineapp.theming
+
+import androidx.compose.ui.graphics.Color
+
+object BrandColors {
+
+    val Anthracite = Color(0xFF2B2B2B)
+    val Gold = Color(0xFFD4AF37)
+    val Purple = Color(0xFF5A4A7A)
+
+    // Dark theme accents
+    val DarkOrange = Color(0xFFFB6F17)
+    val VividPurple = Color(0xFF7927FE)
+
+    // Utility (from your Mud theme)
+    val SoftDivider = Color(0xFFD6D6D6)
+    val DarkDivider = Color(0xFF2C2C2C)
+}
