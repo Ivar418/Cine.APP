a657c05dfad7e46801e4bd9cf5c7f3a1e0d53161
ivarv <ivarvisser@hotmail.com>
2026-06-14 21:09:54 +0200

    Improve `openFile` implementation for Web platform using cleaner Web API integrations

--- composeApp/src/wasmJsMain/kotlin/com/ivarvisser/cineapp/Platform.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/ivarvisser/cineapp/Platform.wasmJs.kt
@@ -1,19 +1,28 @@
 package com.ivarvisser.cineapp
 
+import kotlinx.browser.window
+import org.khronos.webgl.Uint8Array
+import org.khronos.webgl.set
+import org.w3c.dom.url.URL
+import org.w3c.files.Blob
+import org.w3c.files.BlobPropertyBag
+
 class WasmPlatform: Platform {
     override val name: String = "Web with Kotlin/Wasm"
     override val isMobile: Boolean = false
+
+    @OptIn(ExperimentalWasmJsInterop::class)
     override fun openFile(bytes: ByteArray, fileName: String) {
-        val uint8Array = org.khronos.webgl.Uint8Array(bytes.size)
+        val uint8Array = Uint8Array(bytes.size)
         for (i in bytes.indices) {
             uint8Array[i] = bytes[i]
         }
-        val blob = org.w3c.files.Blob(
-            arrayOf(uint8Array),
-            org.w3c.files.BlobPropertyBag(type = "application/pdf")
+        val blob = Blob(
+            arrayOf<JsAny?>(uint8Array).toJsArray(),
+            BlobPropertyBag(type = "application/pdf")
         )
-        val url = org.w3c.dom.url.URL.createObjectURL(blob)
-        kotlinx.browser.window.open(url, "_blank")
+        val url = URL.createObjectURL(blob)
+        window.open(url, "_blank")
     }
 }
 
