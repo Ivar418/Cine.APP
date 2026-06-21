7b95c89cb9f383c7f261a8853e6b1c6f97c9e530
ivarv <ivarvisser@hotmail.com>
2026-06-21 10:43:51 +0200

    Unified multiplatform implementations and removed unneeded expect/actuals

--- composeApp/src/desktopMain/kotlin/com/ivarvisser/cineapp/ui/component/openPaymentUrl.desktop.kt
+++
@@ -1,7 +0,0 @@
-package com.ivarvisser.cineapp.ui.component
-
-import androidx.compose.ui.platform.UriHandler
-
-actual fun openPaymentUrl(uriHandler: UriHandler, url: String) {
-    uriHandler.openUri(url)
-}
